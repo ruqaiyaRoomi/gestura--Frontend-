a4389b4dad6c6ee524bf1132f0d74b7f84833f20
RuqaiyaRoomi <ruqaiyaroomi.com>
2026-04-14 23:26:59 +0400

    added comments

--- gestura-frontend/src/stores/user.js
+++ gestura-frontend/src/stores/user.js
@@ -1,19 +1,25 @@
+// Importing Pinia's defineStore to create a global state store
 import { defineStore } from "pinia";
+// Importing ref from Vue for reactive state variables
 import { ref } from "vue";
 
 export const useUserStore = defineStore('user',  () => {
+    // Stores the currently authenticated user object (null if not logged in)
     const user = ref(null)
+    // Stores full sue activity history
     const history = ref([])
+    // Stores recently signed ASL letters for quick access display
     const recentlySigned = ref([])
-
+    
+    // sets data after successful login or signup
     function setUser(userData) {
         user.value = userData
     }
-
+    // Resets the authenication state across the application
     function logout() {
         user.value = null
     }
-
+    // Returing state varibles and functions to components that use this store
     return {user, setUser, logout, history, recentlySigned}
 })
 
